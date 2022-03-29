052f6d909ee5fc2a612897cff49700ba0db44372
Clarel Salassa <clarelsalassapro@gmail.com>
2022-03-30 01:04:20 +0200

    Adding parser class and GET scheme

---
+++ src/main/java/fr/lernejo/navy_battle/Parser.java
@@ -0,0 +1,52 @@
+package fr.lernejo.navy_battle;
+
+
+import com.sun.net.httpserver.HttpExchange;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Parser {
+
+    public boolean isValidBody(JSONObject jsonObject) {
+        try (InputStream inputStream = getClass().getResourceAsStream("/schemaPost.json")) {
+            JSONObject inputSchema = new JSONObject(new JSONTokener(inputStream));
+            Schema schema = SchemaLoader.load(inputSchema);
+            try {
+                schema.validate(jsonObject);
+                return true;
+            }
+            catch (ValidationException e) {
+                System.err.println("Invalid schema" + e);
+            }
+        }
+        catch (IOException e) {
+            System.err.println(e);
+        }
+        return false;
+    }
+
+    public JSONObject getRequest(HttpExchange exchange){
+        try (InputStream os = exchange.getRequestBody()){
+            BufferedReader rStream = new BufferedReader(new InputStreamReader(os, StandardCharsets.UTF_8));
+            StringBuilder stringBuilder = new StringBuilder();
+            String input;
+            while ((input = rStream.readLine()) != null)
+                stringBuilder.append(input);
+            return new JSONObject(stringBuilder.toString());
+        }
+        catch (IOException e){
+            return null;
+        }
+    }
+
+
+}
